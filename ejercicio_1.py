a9710e61e27b4584a504440babe607f11801bf9f
kersaki <marieteceu@gmail.com>
2022-10-27 19:51:30 +0200

    ejercicios

--- ejercicio_1.py
+++ ejercicio_1.py
@@ -1,6 +1,7 @@
 lista_numeros = [18, 50, 210, 80, 145, 333, 70, 30]
 for i in lista_numeros:
-    if i%10 or i < 200:
+    if i%10 and i < 200:
         print(i)
+        
 
     
---
+++ ejercicio_2.py
@@ -0,0 +1,15 @@
+class Alumno():
+    def __init__(self,nombre,nota):
+        self.nombre = nombre
+        self.nota = nota
+    
+    def __str__(self):
+        return print('Se ha creado con exito'.format(self.nombre))
+
+    def aprobado (nota):
+        if nota < 5:
+            print('Has suspendido')
+        else:
+            print('Has suspendido')
+
+Alumno = [{'Mario', 10},{'Juan', 6},{'Alvaro', 10}]
